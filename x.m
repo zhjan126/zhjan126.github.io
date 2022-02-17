aef59e98cb52d9728060e22f517d7cd973d5cc6a
zhjan126 <99806053+zhjan126@users.noreply.github.com>
2022-02-17 20:45:42 +0800

    Create x.m

---
+++ x.m
@@ -0,0 +1,3 @@
+#EXTM3U
+#EXTINF:-1 tvg-id="141" tvg-name="凤凰中文" tvg-logo="http://epg.51zmt.top:8000/tb1/gt/fenghuangzhongwen.png" group-title="港澳台1",凤凰中文
+http://223.110.235.2/ott.js.chinamobile.com/PLTV/3/224/3221228057/index.m3u8
